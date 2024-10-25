fb61f8af50f9ab80d10a26412a5b2420e4fce63f
1717854 <1717854@mukilteo.wednet.edu>
2024-10-25 13:25:55 -0700

    starting

---
+++ Advanced Text Adventure/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public abstract class Item
+{
+
+	
+
+
+
+    public int itemPrice;
+	public string itemName;
+	public int sellPrice;
+	public string itemDescription;
+
+	public Item ( int itemPrice, string itemName, int sellPrice,string itemDescription)
+	{
+		this.itemPrice = itemPrice;
+		this.itemName = itemName;
+	    this.sellPrice = sellPrice;
+		this.itemDescription = itemDescription;
+
+	}
+
+}
